ffd14be819a776435d744b758410037460b1c13f
Henry Boles <henryjboles@gmail.com>
2021-10-01 08:22:24 -0700

    Make it even funnier

--- src/main/java/top/penowl/quidproquo/rituals/GetLuckyRitual.java
+++ src/main/java/top/penowl/quidproquo/rituals/GetLuckyRitual.java
@@ -14,13 +14,14 @@ public class GetLuckyRitual extends Ritual {
         addIngredient(Material.GOLD_BLOCK, 3);
         addIngredient(Material.TNT, 1);
         health = 3;
+        lightning = true;
         name = "daring";
     }
 
     @Override
     public void execute(Player caster, Player target, Location location) {
         if (Math.random() < 0.5) {
-            location.getWorld().createExplosion(location, 100);
+            location.getWorld().createExplosion(location, 100F);
         } else { 
             byproducts.add(new ItemStack(Material.GOLD_BLOCK, 16));
         }
