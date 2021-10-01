670a31d73ac3468fd8295db10a2815c8165b42f5
Henry Boles <henryjboles@gmail.com>
2021-10-01 08:15:14 -0700

    Add funny ritual

---
+++ src/main/java/top/penowl/quidproquo/rituals/GetLuckyRitual.java
@@ -0,0 +1,29 @@
+package top.penowl.quidproquo.rituals;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import top.penowl.quidproquo.Ritual;
+
+public class GetLuckyRitual extends Ritual {
+
+    @Override
+    public void setup() {
+        addIngredient(Material.GOLD_BLOCK, 3);
+        addIngredient(Material.TNT, 1);
+        health = 3;
+        name = "daring";
+    }
+
+    @Override
+    public void execute(Player caster, Player target, Location location) {
+        if (Math.random() < 0.5) {
+            location.getWorld().createExplosion(location, 100);
+        } else { 
+            byproducts.add(new ItemStack(Material.GOLD_BLOCK, 16));
+        }
+    }
+    
+}
