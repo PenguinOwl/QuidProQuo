000c9561b5f94e2809d915173b1da36a6340b6f3
B1G-FUNGUS <myceliamongi@gmail.com>
2021-10-01 23:12:27 -0700

    Finished the soft penor ritual
    Encases a (targeted) player with a dirt penor.

--- src/main/java/top/penowl/quidproquo/rituals/SoftPenorRitual.java
+++ src/main/java/top/penowl/quidproquo/rituals/SoftPenorRitual.java
@@ -1,8 +1,9 @@
 package top.penowl.quidproquo.rituals;
-//TODO MAKE A DIRT PENOR THAT CAN SPAWN AROUND OTHERS
+//TODO TEST THE PENOR
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import top.penowl.quidproquo.Ritual;
@@ -21,7 +22,34 @@ public class SoftPenorRitual extends Ritual {
 
     @Override
     public void execute(Player caster, Player target, Location location) {
-        Location loc = target.getLocation();
+        Block shaft = target.getLocation().getBlock();
+        Block ball1 = shaft.getRelative(2, -1, 0);
+        Block ball2 = shaft.getRelative(-2,-1,0);
+        //TODO USE ELEGANT MATHEMATICS INSTEAD OF SCUFFED IF STATEMENTS
+        for(int d = 0; d < 8; d ++) {
+            int x = 0;
+            if(d < 3) x = 1;
+            if(d > 4) x = -1;
+            int z = 0;
+            if(d % 3 == 0) z = 1;
+            if(d % 3 == 1) z = -1;
+            for(int y = 0; y < 5; y++) {
+                Block changingBlock = shaft.getRelative(x,y,z);
+                if(changingBlock.getType() == Material.AIR)
+                    changingBlock.setType(Material.DIRT);
+            }
+        }
+        for(int x = 0; x < 3; x ++) {
+            for(int y = 0; y < 3; y ++) {
+                for(int z = 0; z < 3; z ++) {
+                    Block changingBlock1 = ball1.getRelative(x,y,z);
+                    Block changingBlock2 = ball2.getRelative(x,y,z);
+                    if(changingBlock1.getType() == Material.AIR)
+                        changingBlock1.setType(Material.DIRT);
+                    if(changingBlock2.getType() == Material.AIR)
+                        changingBlock1.setType(Material.DIRT);
+                }
+            }
+        }
     }
-    
 }
