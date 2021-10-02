2ef3a26b836441c0079fb47acec1db9244994380
Henry Boles <henryjboles@gmail.com>
2021-10-01 21:02:50 -0700

    Change colors

--- src/main/java/top/penowl/quidproquo/Commands.java
+++ src/main/java/top/penowl/quidproquo/Commands.java
@@ -72,7 +72,7 @@ public class Commands implements CommandExecutor {
                 builder.append(": " + ChatColor.AQUA + entry.getValue().toString());
                 builder.append("\n");
             }
-            builder.append(" \n" + ChatColor.AQUA + "" + ChatColor.BOLD + "Blood: " + ChatColor.RESET + "" + ChatColor.AQUA + String.valueOf(ritual.health / 2.0) + ChatColor.RED + "♥\n");
+            builder.append(" \n" + ChatColor.GOLD + "" + ChatColor.BOLD + "Blood: " + ChatColor.RESET + "" + ChatColor.AQUA + String.valueOf(ritual.health / 2.0) + ChatColor.RED + "♥\n");
             builder.append(ChatColor.GOLD + "" + ChatColor.BOLD + "Backfire: " + ChatColor.RESET + "" + ChatColor.AQUA + String.valueOf(Math.round(ritual.backfire * 100)) + ChatColor.GREEN + "%\n");
             sender.sendMessage(builder.toString());
             return true;
