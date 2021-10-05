fc2b307c3c3e182b4d2f906a260a9ab44ab16264
Henry Boles <henryjboles@gmail.com>
2021-10-04 17:03:22 -0700

    Funny ritual

--- src/main/java/top/penowl/quidproquo/rituals/FakePlayerRitual.java
+++ src/main/java/top/penowl/quidproquo/rituals/FakePlayerRitual.java
@@ -22,6 +22,7 @@ public class FakePlayerRitual extends Ritual {
         addIngredient(Material.LEAVES, 32);
         name = "player illusion";
         notify = false;
+        health = 4;
     }
 
     @Override
@@ -32,13 +33,15 @@ public class FakePlayerRitual extends Ritual {
         int y = random.nextInt(51) - 25;
         int z = random.nextInt(51) - 25;
         Entity fakePlayer = target.getWorld().spawnEntity(loc.clone().add(x, y, z), EntityType.SLIME);
-        fakePlayer.setCustomName(caster.getName());
         LivingEntity fp_livingEntity = (LivingEntity)fakePlayer;
         fp_livingEntity.setMaxHealth(100.0);
         fp_livingEntity.setHealth(100.0);
         fp_livingEntity.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 100));
         fp_livingEntity.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 200, 1));
-        fp_livingEntity.setCustomNameVisible(true);
+        Entity armorStand = target.getWorld().spawnEntity(loc.clone().add(x, y, z), EntityType.ARMOR_STAND);
+        armorStand.setCustomName(caster.getName());
+        armorStand.setCustomNameVisible(true);
+        fp_livingEntity.setPassenger(armorStand);
         Slime fpSlime = (Slime)fp_livingEntity;
         fpSlime.setSize(1);
     }
