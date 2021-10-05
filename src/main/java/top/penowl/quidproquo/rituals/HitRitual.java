977d690dd0e7a26379d6ecf91701b698d310a3bd
B1G-FUNGUS <myceliamongi@gmail.com>
2021-10-04 18:07:33 -0700

    added some delays for the hit ritual

--- src/main/java/top/penowl/quidproquo/rituals/HitRitual.java
+++ src/main/java/top/penowl/quidproquo/rituals/HitRitual.java
@@ -1,10 +1,12 @@
 package top.penowl.quidproquo.rituals;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
+import top.penowl.quidproquo.QuidProQuo;
 import top.penowl.quidproquo.Ritual;
 
 public class HitRitual extends Ritual {
@@ -21,6 +23,16 @@ public class HitRitual extends Ritual {
     @Override
     public void execute(Player caster, Player target, Location location) {
         target.playSound(target.getLocation(), Sound.HURT_FLESH, 1, 1);
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(QuidProQuo.instance, new Runnable() {
+            public void run() {
+                target.playSound(target.getLocation(), Sound.HURT_FLESH, 1, 1);
+            }
+        }, 1 * 15L);
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(QuidProQuo.instance, new Runnable() {
+            public void run() {
+                target.playSound(target.getLocation(), Sound.HURT_FLESH, 1, 1);
+            }
+        }, 1 * 15L);
     }
 
 }
