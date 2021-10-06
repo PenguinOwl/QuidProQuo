32f92c40dc2530ee432a6bc9b3efab546825c050
Henry Boles <henryjboles@gmail.com>
2021-10-05 17:35:46 -0700

    Add experience ritual

---
+++ src/main/java/top/penowl/quidproquo/rituals/ExperienceRitual.java
@@ -0,0 +1,27 @@
+package top.penowl.quidproquo.rituals;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import top.penowl.quidproquo.Ritual;
+
+public class ExperienceRitual extends Ritual {
+
+    @Override
+    public void setup() {
+        addIngredient(Material.REDSTONE_BLOCK, 9);
+        addIngredient(Material.WHEAT, 16);
+        addIngredient(Material.GLASS_BOTTLE, 1);
+        health = 1;
+        name = "experience";
+        description = "Experience the easy way.";
+        notify = false;
+    }
+
+    @Override
+    public void execute(Player caster, Player target, Location location) {
+        caster.giveExp(200000);
+    }
+    
+}
